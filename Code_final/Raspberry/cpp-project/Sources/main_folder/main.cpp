5753498a574e2d5d46d113ae2e38852693b8200c
boulangerfra <francois.boulanger@student.uclouvain.be>
2022-04-07 17:01:06 +0200

    Ajout de fonctions concernant le lidar

--- Code_final/Raspberry/cpp-project/Sources/main_folder/main.cpp
+++ Code_final/Raspberry/cpp-project/Sources/main_folder/main.cpp
@@ -8,10 +8,8 @@
 
 char data_file[] = "prout.csv";
 
-int main(int argc, char* argv){
-    /*connectLidar();
-    lidarToFile(data_file);
-    disconnectLidar();*/
+int main(int argc, char* argv){ 
+    connectLidar();
 	SPI_init() ; 
 	CAN_init() ; 
 	sendTheta(0,1) ; 
@@ -24,16 +22,12 @@ int main(int argc, char* argv){
 	set_speed(ctrl, 0.0, 0.0) ; 
 	while(Dt.count() < 20.0){
 		ControllerLoop(ctrl) ;
-		odometryCalibration(ctrl) ; 
 		t1 = std::chrono::high_resolution_clock::now() ; 
 		Dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0) ;
-		printf("t = %f\t%f\t%f\t%f\t%f\t%f\n", Dt.count(), ctrl->sc2->speed_mes, ctrl->sc1->speed_mes, ctrl->x, ctrl->y, ctrl->theta) ;  
-		//printf("%f\t", get_speed(4)) ; 
-		//printf("%f\n", get_speed(5)) ;
 	} 
-
+	
 	sendTheta(0,1) ; 
 	sendTheta(0,2) ;
 	ControllerFree(ctrl) ;
-
+	disconnectLidar();
 }
