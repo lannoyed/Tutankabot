1c7b9f5678389e78eee48afe0c29026b1ee8f8fc
Théau Bernard <theaub@hotmail.be>
2022-04-20 20:07:30 +0200

    Comm arduino terminée

--- Code_final/Raspberry/cpp-project/Sources/arduino-comm/arduino-comm.h
+++ Code_final/Raspberry/cpp-project/Sources/arduino-comm/arduino-comm.h
@@ -11,3 +11,25 @@ int arduinoSerialDisconnect();
 void sendSerialChar(unsigned char c);
 
 int sendScore(unsigned char score);
+
+int pushReplica();
+
+int pushExcavationSquare();
+
+int goalUp();
+
+int goalDown();
+
+int yarmUp();
+
+int yarmDown();
+
+int probesUp();
+
+int probesDown();
+
+int gripperClose();
+
+int gripperOpen();
+
+int readResistance();
