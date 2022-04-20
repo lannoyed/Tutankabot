a4af93c40ea12a72d252c870e2c04d3e84f037b3
boulangerfra <francois.boulanger@student.uclouvain.be>
2022-04-20 09:32:50 +0200

    Merge main

--- Code_final/Raspberry/cpp-project/Sources/main_folder/main.cpp
+++ Code_final/Raspberry/cpp-project/Sources/main_folder/main.cpp
@@ -3,12 +3,54 @@
 #include <unistd.h>
 #include "lidar.h"
 #include "SPICom.h"
-#include "canCom.h"
 #include "FSM.h"
 
 char data_file[] = "prout.csv";
+// attention réactiver lidar ^^^^^^
 
 
+
+
+
+void lidar_loop (std::atomic_bool& running,  Controller* ctrl){
+ 
+ std::chrono::high_resolution_clock::time_point last_lidar_update = std::chrono::high_resolution_clock::now();
+
+ FILE * myThread;
+ myThread = fopen("myThread.txt", "w"); 
+ fprintf(myThread, "iteration init \n" );
+ 
+ int i = 0;
+ while (running )
+ {
+	//update_lidar_data(last_lidar_update, ctrl->lidar_angles, ctrl->lidar_distance, ctrl->lidar_quality);
+ 
+  fprintf(myThread, "iteration %d \n", i);
+  i++;
+  
+  //std::cout<< "lock opo \n";
+	
+  update_opponent_location(ctrl);
+	
+
+
+  //std::cout<< "lock our \n";
+	ctrl->LockLidarOurPosition.lock();
+	//triangulation(ctrl);
+  double x_lidar;
+  double y_lidar;
+  double theta_lidar;
+	ctrl->LockLidarOurPosition.unlock();
+ 
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+ }
+ 
+ fprintf(myThread, "end threads \n");
+
+ //fclose(myThread);
+ 
+}
 int main(int argc, char* argv){ 
 	//
 	FILE* mainLog;
@@ -21,18 +63,15 @@ int main(int argc, char* argv){
   
 	SPI_init() ;
 	std::cout<< "SPI initialized"<<"\n";  
-
-	CAN_init() ; 
-	std::cout<< "CAN initialized"<<"\n";
-  
-	sendTheta(0,1) ; 
-	sendTheta(0,2) ; 
-	std::cout<< "0 0 send to motors"<<"\n";   
+ 
  
 	Controller* ctrl ; 
 	ctrl = ControllerInit() ;
 	std::cout<< "Controleur initialized"<<"\n"; 
-  
+	
+	sendTheta(0,1) ; 
+	sendTheta(0,2) ; 
+	std::cout<< "0 0 send to motors"<<"\n";
 	FSM_init(ctrl);
 	std::cout<< "FSM initialized"<<"\n"; 
   
@@ -40,32 +79,33 @@ int main(int argc, char* argv){
 	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now() ; 
 	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now() ;
 	std::chrono::duration<double> Dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0) ;
-	std::cout<<"set speed"<<"\n"; 
+	
 	set_speed(ctrl, 0.0, 0.0);
+	std::cout<<"set speed"<<"\n"; 
  
 	double deltaT = 0.0;
-	while(Dt.count() < 27.0){
-		if(Dt.count() < 27.0){
-			ControllerLoop(ctrl) ;
-			printf("%f     %f\n", ctrl->x, ctrl->y) ;	
-		}
-		//FSM_loop(ctrl, 0.17);
-		//std::cout<<" here\n";
-   
 
-		//
-		//
-		//fprintf(mainLog,"%f %f %f %f\n",ctrl->sc1->speed_mes, ctrl->sc2->speed_mes, ctrl->x, ctrl->y);
+	std::atomic<bool> running (true) ;
+	std::thread lidar_thread(lidar_loop, std::ref(running), ctrl);
+ 
+	std::cout<<"Threads created"<<"\n"; 
+
+	while(Dt.count() < 40.0){
+ 
+		FSM_loop(ctrl, 0.17);
+		std::cout<<" here\n";
+   
+		fprintf(mainLog,"%f %f %f %f\n",ctrl->sc1->speed_mes, ctrl->sc2->speed_mes, ctrl->x, ctrl->y);
 		t2 = t1;
 		t1 = std::chrono::high_resolution_clock::now() ; 
 		deltaT = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t2).count();
-		//std::cout<<"local time:   \t" << deltaT << "\n"; 
-		//std::cout<<"globale time: \t" << Dt.count() << "\n";
+		std::cout<<"local time:   \t" << deltaT << "\n"; 
+		std::cout<<"globale time: \t" << Dt.count() << "\n";
 		Dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0) ;
-		//std::cout<<"sleep \n";
+		std::cout<<"sleep \n";
+	
     
-    
-		/*double time_to_wait = 0.028 - deltaT;
+		double time_to_wait = 0.1 - deltaT;
 		if ( time_to_wait < 0) {
 			std::cout  << "negative sleep : " << time_to_wait << "\n";
 			time_to_wait = 0.0;
@@ -73,8 +113,16 @@ int main(int argc, char* argv){
        
 		sleep(time_to_wait); 
     
-		//*/
+		//
 	}  
+ 
+	std::cout <<" before running \n";
+	running = false;
+	std::cout <<" before joint \n";
+  
+	lidar_thread.join();
+	std::cout <<"joint \n";
+  
 	t1 = std::chrono::high_resolution_clock::now() ; 
 	Dt = std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0) ;
 	std::cout<< "end phase "<<"\n";
